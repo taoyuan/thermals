0cfe495f6d870798b4c8fb3865cf3fef692b5e12
taoyuan <torworx@gmail.com>
2015-09-04 18:45:28 +0800

    update

--- examples/simple.js
+++ examples/simple.js
@@ -4,9 +4,6 @@ var Printer = require('../');
 
 var printer = new Printer('escpos', 'file', '/dev/usb/lp0');
 
-printer.open().then(function () {
-
-  printer.write('hello, 你好').print().then(function () {
-    printer.close();
-  });
+printer.write('hello, 你好').print(function () {
+  console.log('Write OK!');
 });
