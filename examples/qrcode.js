c8c410ac296f9068e1448d80d2a0082ba57b6544
taoyuan <towyuan@outlook.com>
2015-09-04 21:19:36 +0800

    update

--- examples/qrcode.js
+++ examples/qrcode.js
@@ -4,6 +4,7 @@ var Printer = require('../');
 
 var printer = new Printer('escpos', 'file', '/dev/usb/lp0');
 
-printer.qrCode('Hello').print(function () {
-  console.log('Write OK!');
-});
+printer
+  .qrCode('Hello')
+  .write('\n')
+  .print();
--- examples/simple.js
+++ examples/simple.js
@@ -4,6 +4,4 @@ var Printer = require('../');
 
 var printer = new Printer('escpos', 'file', '/dev/usb/lp0');
 
-printer.write('Hello, 你好').print(function () {
-  console.log('Write OK!');
-});
+printer.write('你好，世界\n').print();
