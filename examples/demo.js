372fdc63e86bf0dc363db430189dfd3955217530
taoyuan <torworx@gmail.com>
2015-09-05 18:28:53 +0800

    update

--- examples/demo.js
+++ examples/demo.js
@@ -142,7 +142,7 @@ function demo_barcodes() {
 
 ///* Graphics - this demo will not work on some non-Epson printers */
 function demo_graphics() {
-  return Printer.image("./resources/thermals.png").then(function (img) {
+  return Printer.image("./resources/sample.png").then(function (img) {
     ['default', 'double-width', 'double-height', 'double'].forEach(function (size) {
       printer.graphics(img, size);
     });
