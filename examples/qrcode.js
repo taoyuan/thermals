9cd009b6a27f684c36a728f1d04058331dd104f9
taoyuan <towyuan@outlook.com>
2015-09-06 14:25:14 +0800

    update

--- examples/qrcode.js
+++ examples/qrcode.js
@@ -2,11 +2,17 @@
 
 var Printer = require('../');
 
-var printer = new Printer();
+//var printer = new Printer();
+var printer = new Printer('file:///dev/usb/lp1');
 
-var str = 'Hello';
+var str = 'Hello World!';
+
+var version = 10;
 
 printer
-  .qr(str)
-  .feed()
+  .setAlign('center')
+  .qr(str, {level: 'h', version: version})
+  .text(str)
+  .feed(10)
+  .cut()
   .print();
